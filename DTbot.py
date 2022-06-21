3f4479a575c88ab5059d8be0913124b1720292e9
Major Tanya <github@majortanya.eu>
2022-06-21 17:35:50 +0200

    Prevent nextcord from syncing Slash Commands
    These kwargs and the method override are necessary to prevent nextcord from trying to sync (and thus delete from Discord) Slash Commands declared by the Slash Command-based v3 instance.

--- DTbot.py
+++ DTbot.py
@@ -23,12 +23,15 @@ log_startup_time = startup_time.strftime('%Y-%m-%d (%H-%M-%S %Z)')
 
 class DTbot(commands.Bot):
     def __init__(self, det_prefixes=None):
-        super().__init__(case_insensitive=True, command_prefix=det_prefixes, intents=intents)
+        super().__init__(case_insensitive=True, command_prefix=det_prefixes, intents=intents,
+                         # prevent application command overrides due to this being v2 (classic)
+                         rollout_associate_known=False, rollout_delete_unknown=False, rollout_register_new=False,
+                         rollout_update_known=False)
         self.dtbot_colour = nextcord.Colour(0x5e51a8)
         self.remove_command('help')
         # set up logging and bind to instance
         self.log = logging.getLogger('nextcord')
-        self.log.setLevel('WARN')
+        self.log.setLevel(logging.WARNING)
         handler = logging.FileHandler(filename=f'./logs/{log_startup_time}.log', encoding='utf-8', mode='w')
         handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s'))
         self.log.addHandler(handler)
@@ -40,6 +43,17 @@ class DTbot(commands.Bot):
             except Exception as e:
                 exc = f'{type(e).__name__}: {e}'
                 print(f'Failed to load extension {extension}\n{exc}.')
+
+    # Prevent nextcord's attempts at using Slash Commands (this is v2 (classic), traditional commands only mode)
+
+    async def on_connect(self) -> None:
+        pass
+
+    async def on_guild_available(self, guild: nextcord.Guild) -> None:
+        pass
+
+    async def on_interaction(self, interaction: nextcord.Interaction):
+        pass
 
     async def on_ready(self):
         # online confimation
