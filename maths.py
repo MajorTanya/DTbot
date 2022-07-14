417a28d0997126b5d2d746d6f4e2c073570c2d92
MajorTanya <github@majortanya.eu>
2022-07-15 01:26:27 +0200

    Migrate Maths cog to use Slash Commands
    This is a GroupCog, meaning all its Slash Commands will start with /maths, e.g. add will be /maths add, etc.

--- maths.py
+++ maths.py
@@ -1,58 +1,51 @@
-import re
-
+import discord
+from discord import app_commands
 from discord.ext import commands
 
 from DTbot import DTbot
 from util.utils import rint
 
 
-class Maths(commands.Cog):
+class Maths(commands.GroupCog):
     """Some mathematical commands"""
 
     def __init__(self, bot: DTbot):
         self.bot = bot
 
-    @commands.command(description="Add two numbers together\n\nUsage:\n+add 19 50",
-                      brief="Addition")
-    async def add(self, ctx: commands.Context, left: float, right: float):
-        await ctx.send(f'{rint(left)} + {rint(right)} = {rint(left + right)}')
+    @app_commands.command(description="Add two numbers together")
+    async def add(self, interaction: discord.Interaction, left: float, right: float):
+        await interaction.response.send_message(f'{rint(left):g} + {rint(right):g} = {rint(left + right)}')
 
-    @commands.command(description="Calculates the square of a number\n\nUsage:\n+square 11",
-                      brief="Squaring")
-    async def square(self, ctx: commands.Context, number: float):
-        await ctx.send(f'{rint(number)}² = {rint(pow(number, 2))}')
+    @app_commands.command(description="Calculates the square of a number")
+    async def square(self, interaction: discord.Interaction, number: float):
+        await interaction.response.send_message(f'{rint(number)}² = {rint(pow(number, 2))}')
 
-    @commands.command(description="Subtract two numbers\n\nUsage:\n+subtract 99 30",
-                      brief="Subtraction")
-    async def subtract(self, ctx: commands.Context, left: float, right: float):
-        await ctx.send(f'{rint(left)} - {rint(right)} = {rint(left - right)}')
+    @app_commands.command(description="Subtract two numbers")
+    async def subtract(self, interaction: discord.Interaction, first: float, second: float):
+        await interaction.response.send_message(f'{rint(first)} - {rint(second)} = {rint(first - second)}')
 
-    @commands.command(description="Multiply two numbers\n\nUsage:\n+multiply 35 7",
-                      brief="Multiplication")
-    async def multiply(self, ctx: commands.Context, left: float, right: float):
-        await ctx.send(f'{rint(left)} * {rint(right)} = {rint(left * right)}')
+    @app_commands.command(description="Multiply two numbers")
+    async def multiply(self, interaction: discord.Interaction, first: float, second: float):
+        await interaction.response.send_message(f'{rint(first)} * {rint(second)} = {rint(first * second)}')
 
-    @commands.command(description="Divide a number by another number\n\nUsage:\n+divide 65 5",
-                      brief="Division")
-    async def divide(self, ctx: commands.Context, dividend: float, divisor: float):
+    @app_commands.command(description="Divide a number by another number")
+    async def divide(self, interaction: discord.Interaction, dividend: float, divisor: float):
         if divisor == 0:
-            await ctx.send('Division by 0 is not allowed.')
+            await interaction.response.send_message('Division by 0 is not allowed.', ephemeral=True)
         else:
-            await ctx.send(f'{rint(dividend)} / {rint(divisor)} = {rint(dividend / divisor)}')
+            await interaction.response.send_message(f'{rint(dividend)} / {rint(divisor)} = {rint(dividend / divisor)}')
 
-    @commands.command(description="Calculate a percentage\n\nUsage: (Result: 25%)\n+percentage 15 60",
-                      brief="Percentage")
-    async def percentage(self, ctx: commands.Context, part: float, whole: float):
+    @app_commands.command(description="Calculate a percentage (15 apples of 60? It's 25%)")
+    async def percentage(self, interaction: discord.Interaction, part: float, whole: float):
         if whole == 0:
-            await ctx.send("Can't have a percentage of an empty whole")
+            await interaction.response.send_message("Can't have a percentage of an empty whole")
         else:
-            await ctx.send(f'{rint(part)} / {rint(whole)} = {part / whole:.2%}')
+            await interaction.response.send_message(f'{rint(part)} / {rint(whole)} = {rint(part / whole):.2%}')
 
-    @commands.command(description="Calculate how much a percentage equates to\n\nUsage: (Result: 15)\n+percentof 25 60",
-                      brief="Percent of")
-    async def percentof(self, ctx: commands.Context, percentage, whole: float):
-        percentage_re = float(re.sub('[^0-9.]', '', percentage))
-        await ctx.send(f'{rint(percentage_re)}% of {rint(whole)} = {rint((percentage_re / 100) * whole)}')
+    @app_commands.command(description="Calculate how much a percentage equates to (25% of 60? It's 15)")
+    async def percentof(self, interaction: discord.Interaction, percentage: float, whole: float):
+        await interaction.response.send_message(f'{rint(percentage)}% of {rint(whole)}'
+                                                f' = {rint((percentage / 100) * whole)}')
 
 
 async def setup(bot: DTbot):
