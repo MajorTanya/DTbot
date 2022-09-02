3aa7d5993769f07da57a1a5a987a68ee568f8e8f
Major Tanya <github@majortanya.eu>
2022-09-02 02:42:50 +0200

    Fix Python 3.8+ (in)compatability
    README.md previously claimed that DTbot v2 can be run with Python 3.8+, but we used the X|Y Union syntax, which is a 3.10 feature. Adding `from __future__ import annotations` has fixed this mistake.

--- util/utils.py
+++ util/utils.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 def rint(flt: float) -> int | float:
     """Round to 2 digits. Returns int if rounded float has only zeroes after the decimal point."""
     return int(rounded) if (rounded := round(flt, 2)).is_integer() else rounded
