e310cf673e6dfdb4b6c2d1f7aa4fd96aeea85197
MajorTanya <github@majortanya.eu>
2023-05-04 17:15:37 +0200

    Add thousands separator to Maths commands
    As DTbot is (mostly) set up to work in British English, a comma is used as the thousands separator, and a dot is used as the decimal separator. This keeps the numbers displayed consistent.
    
    Also removed leftover 'g' formatter from /maths add.

--- maths.py
+++ maths.py
@@ -14,38 +14,40 @@ class Maths(commands.GroupCog):
 
     @app_commands.command(description="Add two numbers together")
     async def add(self, interaction: discord.Interaction, left: float, right: float):
-        await interaction.response.send_message(f"{rint(left):g} + {rint(right):g} = {rint(left + right)}")
+        await interaction.response.send_message(f"{rint(left):,} + {rint(right):,} = {rint(left + right):,}")
 
     @app_commands.command(description="Calculates the square of a number")
     async def square(self, interaction: discord.Interaction, number: float):
-        await interaction.response.send_message(f"{rint(number)}² = {rint(pow(number, 2))}")
+        await interaction.response.send_message(f"{rint(number):,}² = {rint(pow(number, 2)):,}")
 
     @app_commands.command(description="Subtract two numbers")
     async def subtract(self, interaction: discord.Interaction, first: float, second: float):
-        await interaction.response.send_message(f"{rint(first)} - {rint(second)} = {rint(first - second)}")
+        await interaction.response.send_message(f"{rint(first):,} - {rint(second):,} = {rint(first - second):,}")
 
     @app_commands.command(description="Multiply two numbers")
     async def multiply(self, interaction: discord.Interaction, first: float, second: float):
-        await interaction.response.send_message(f"{rint(first)} * {rint(second)} = {rint(first * second)}")
+        await interaction.response.send_message(f"{rint(first):,} * {rint(second):,} = {rint(first * second):,}")
 
     @app_commands.command(description="Divide a number by another number")
     async def divide(self, interaction: discord.Interaction, dividend: float, divisor: float):
         if divisor == 0:
             await interaction.response.send_message("Division by 0 is not allowed.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{rint(dividend)} / {rint(divisor)} = {rint(dividend / divisor)}")
+            await interaction.response.send_message(
+                f"{rint(dividend):,} / {rint(divisor):,} = {rint(dividend / divisor):,}"
+            )
 
     @app_commands.command(description="Calculate a percentage (15 apples of 60? It's 25%)")
     async def percentage(self, interaction: discord.Interaction, part: float, whole: float):
         if whole == 0:
             await interaction.response.send_message("Can't have a percentage of an empty whole")
         else:
-            await interaction.response.send_message(f"{rint(part)} / {rint(whole)} = {rint(part / whole, 4):.2%}")
+            await interaction.response.send_message(f"{rint(part):,} / {rint(whole):,} = {rint(part / whole, 4):,.2%}")
 
     @app_commands.command(description="Calculate how much a percentage equates to (25% of 60? It's 15)")
     async def percentof(self, interaction: discord.Interaction, percentage: float, whole: float):
         result = rint((percentage / 100) * whole)
-        await interaction.response.send_message(f"{rint(percentage)}% of {rint(whole)} = {result}")
+        await interaction.response.send_message(f"{rint(percentage):,}% of {rint(whole):,} = {result:,}")
 
 
 async def setup(bot: DTbot):
