b26d492cdd9057a696eb3eace24599ef57626e94
MajorTanya <github@majortanya.eu>
2024-05-22 00:28:37 +0200

    Remove type: ignore from set difference in /dance
    Appears to be fine the way it is now. Also fixed the set typing.

--- interaction.py
+++ interaction.py
@@ -96,8 +96,8 @@ class Interaction(commands.Cog):
         user4: discord.Member | None,
         user5: discord.Member | None,
     ):
-        # Remove None and Invoker by set difference, type ignored because set difference isn't analysed
-        dancers: set[Member] = {user1, user2, user3, user4, user5}.difference({None, interaction.user})  # type: ignore
+        # Remove None and Invoker by set difference
+        dancers: set[discord.Member] = {user1, user2, user3, user4, user5}.difference({None, interaction.user})
         msg = f"{interaction.user.mention} started dancing by themselves! Everyone, come and join them! DANCE PARTY!"
         if len(dancers) > 0:
             res = ", and ".join(c.mention for c in dancers)
