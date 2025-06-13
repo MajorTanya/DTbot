57170bc1e6877636b9d61c26e6f230ee30ce6ece
MajorTanya <github@majortanya.eu>
2025-06-13 22:32:48 +0200

    Prettify procedures.sql
    - Uppercase SQL keywords like BIGINT properly
    - one parameter per line for procedure definitions
    - use one line per logical segment of a SQL query
    
    The last change makes it easier to read the query and understand its
    logical steps, e.g.
    "This query SELECTs x... FROM the table... WHERE this applies".

--- database_scripts/procedures.sql
+++ database_scripts/procedures.sql
@@ -2,63 +2,117 @@ USE `dtbot`;
 
 -- App Command Stats
 
-CREATE PROCEDURE IF NOT EXISTS `AddNewAppCommand`(IN `newCommandName` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin)
+CREATE PROCEDURE IF NOT EXISTS `AddNewAppCommand`(
+    IN `newCommandName` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+)
 BEGIN
-    INSERT IGNORE INTO `appcommandstats` (command_name) VALUES (newCommandName);
+    INSERT IGNORE
+    INTO `appcommandstats` (command_name)
+    VALUES (newCommandName);
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `CheckAppCommandExist`(IN `lookupCommandName` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin)
+CREATE PROCEDURE IF NOT EXISTS `CheckAppCommandExist`(
+    IN `lookupCommandName` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+)
 BEGIN
-    SELECT IFNULL((SELECT TRUE FROM `appcommandstats` WHERE command_name = lookupCommandName LIMIT 1), FALSE);
+    SELECT IFNULL(
+                   (SELECT TRUE
+                    FROM `appcommandstats`
+                    WHERE command_name = lookupCommandName
+                    LIMIT 1),
+                   FALSE);
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `IncrementAppCommandUsage`(IN `targetCommandName` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin)
+CREATE PROCEDURE IF NOT EXISTS `IncrementAppCommandUsage`(
+    IN `targetCommandName` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+)
 BEGIN
-    UPDATE IGNORE `appcommandstats` SET times_used = times_used + 1 WHERE command_name = targetCommandName;
+    UPDATE IGNORE `appcommandstats`
+    SET times_used = times_used + 1
+    WHERE command_name = targetCommandName;
 END;
 
 -- Servers
 
-CREATE PROCEDURE IF NOT EXISTS `AddNewServer`(IN `newServerId` bigint, IN `newServerMembers` int)
+CREATE PROCEDURE IF NOT EXISTS `AddNewServer`(
+    IN `newServerId` BIGINT,
+    IN `newServerMembers` INT
+)
 BEGIN
-    INSERT INTO `servers` (server_id, server_members)
-    VALUES (newServerId, newServerMembers)
-    ON DUPLICATE KEY UPDATE server_members = newServerMembers, bot_in_server = 1;
+    INSERT INTO `servers` (server_id,
+                           server_members)
+    VALUES (newServerId,
+            newServerMembers)
+    ON DUPLICATE KEY UPDATE server_members = newServerMembers,
+                            bot_in_server  = 1;
 END;
 
 CREATE PROCEDURE IF NOT EXISTS `GetServers`()
 BEGIN
-    SELECT server_id FROM `servers` WHERE bot_in_server = 1;
+    SELECT server_id
+    FROM `servers`
+    WHERE bot_in_server = 1;
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `InvalidateMissingServer`(IN `serverIdToInvalidate` bigint)
+CREATE PROCEDURE IF NOT EXISTS `InvalidateMissingServer`(
+    IN `serverIdToInvalidate` BIGINT
+)
 BEGIN
-    UPDATE `servers` SET bot_in_server = 0 WHERE server_id = serverIdToInvalidate;
+    UPDATE `servers`
+    SET bot_in_server = 0
+    WHERE server_id = serverIdToInvalidate;
 END;
 
 -- Users
 
-CREATE PROCEDURE IF NOT EXISTS `AddNewUser`(IN `newUserId` bigint)
+CREATE PROCEDURE IF NOT EXISTS `AddNewUser`(
+    IN `newUserId` BIGINT
+)
 BEGIN
-    INSERT IGNORE INTO `users` (user_id) VALUES (newUserId);
+    INSERT IGNORE
+    INTO `users` (user_id)
+    VALUES (newUserId);
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `CheckUserExist`(IN `lookupUserId` bigint)
+CREATE PROCEDURE IF NOT EXISTS `CheckUserExist`(
+    IN `lookupUserId` BIGINT
+)
 BEGIN
-    SELECT IFNULL((SELECT TRUE FROM `users` WHERE user_id = lookupUserId LIMIT 1), FALSE);
+    SELECT IFNULL(
+                   (SELECT TRUE
+                    FROM `users`
+                    WHERE user_id = lookupUserId
+                    LIMIT 1),
+                   FALSE);
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `CheckXPTime`(IN `lookupUserId` bigint)
+CREATE PROCEDURE IF NOT EXISTS `CheckXPTime`(
+    IN `lookupUserId` BIGINT
+)
 BEGIN
-    SELECT user_last_xp_gain FROM `users` WHERE user_id = lookupUserId LIMIT 1;
+    SELECT user_last_xp_gain
+    FROM `users`
+    WHERE user_id = lookupUserId
+    LIMIT 1;
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `GetUserXp`(IN `lookupUserId` bigint)
+CREATE PROCEDURE IF NOT EXISTS `GetUserXp`(
+    IN `lookupUserId` BIGINT
+)
 BEGIN
-    SELECT IFNULL((SELECT user_xp FROM `users` WHERE user_id = lookupUserId LIMIT 1), 0);
+    SELECT IFNULL(
+                   (SELECT user_xp
+                    FROM `users`
+                    WHERE user_id = lookupUserId
+                    LIMIT 1),
+                   0);
 END;
 
-CREATE PROCEDURE IF NOT EXISTS `IncreaseXP`(IN `targetUserId` bigint, IN `newXp` int, IN `lastXpGain` int)
+CREATE PROCEDURE IF NOT EXISTS `IncreaseXP`(
+    IN `targetUserId` BIGINT,
+    IN `newXp` INT,
+    IN `lastXpGain` INT
+)
 BEGIN
     UPDATE IGNORE `users`
     SET user_xp           = user_xp + newXp,
