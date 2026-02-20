f2ce5d18bd163958ef47b508cadf380523daf86e
MajorTanya <github@majortanya.eu>
2026-02-20 14:05:58 +0100

    Fix self-exclusion logic for /dance
    This was intended to remove the command user so the output doesn't say
    "UserA started dancing with UserA" (like to the other commands), but
    due to an oversight, the bot itself was checked for and removed if it
    was chosen as one of the dancers. (It's fine to dance with the bot).

--- src/cogs/interaction.py
+++ src/cogs/interaction.py
@@ -104,8 +104,8 @@ class Interaction(commands.Cog):
         # Remove None and the bot itself
         raw_dancers = [user1, user2, user3, user4, user5]
         dancers = [dancer for dancer in raw_dancers if dancer is not None]
-        if interaction.guild is not None and interaction.guild.me in dancers:
-            dancers.remove(interaction.guild.me)
+        if not isinstance(interaction.user, discord.User) and interaction.user in dancers:
+            dancers.remove(interaction.user)
 
         msg = f"{interaction.user.mention} started dancing by themselves! Everyone, come and join them! DANCE PARTY!"
         if len(dancers) > 0:
